d069ee2b02214974a5691f6a2cc66200a7d8f985
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-02 19:29:13 -0500

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+# return first n fibonacci numbers
+
+def fibonacci(n):
+    if n < 2:
+        return 1
+    fibList = [1,1]
+    for i in range(2, n):
+        fibList.append(fibList[i-2] + fibList[i-1])
+    return fibList
