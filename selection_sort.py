5a6e18769605e5eb73b0d4567f55e5807f25d27e
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-07-24 22:28:00 -0500

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(l):
+    for i in range(len(l)):
+        for j in range(i, len(l)):
+            if l[j] < l[i]:
+                temp = l[i]
+                l[i] = l[j]
+                l[j] = temp
+    return l
